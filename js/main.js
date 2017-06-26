fbd3c1cd82914cfd11660f86e43289915707af11
Cathleen Ghenne <cathleen@workangel.com>
2017-06-26 20:45:12 +0100

    add bunny board structure

--- js/main.js
+++ js/main.js
@@ -1,4 +1,53 @@
+$(document).ready(function() {
+  var emptyCell = {
+    class: 'empty',
+  };
+  var bunnyCell = {
+    class: 'bunny',
+  };
+  var arrayX = new Array(5);
+  var arrayY = new Array(5);
+  var board = [
+    [bunnyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+    [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+    [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+    [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+    [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+  ];
 
+  var x;
+  var y;
+  // create HTML board
+  for (x = 0; x < arrayX.length; x++) {
+    for (y = 0; y < arrayY.length; y++) {
+      $('.boardgame')
+        .append(
+          '<div id="' + x + '-' + y +
+          '" class="' + board[x][y].class + '"></div>');
+    }
+  }
 
+  whereIsBunny();
+  board[0][0] = emptyCell;
+  board[1][0] = bunnyCell;
+  redrawCell('#0-0', emptyCell);
+  redrawCell('#1-0', bunnyCell);
+  whereIsBunny();
 
+  function redrawCell(id, currentCell) {
+    $(id)
+      .attr('class', '')
+      .addClass(currentCell.class);
+  }
 
+  function whereIsBunny() {
+    for (x = 0; x < arrayX.length; x++) {
+      for (y = 0; y < arrayY.length; y++) {
+        if (board[x][y] === bunnyCell) {
+          console.log('Bunny is on cell ' + x + ' ' + y);
+        }
+      }
+    }
+  }
+
+});
