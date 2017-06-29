0fcd779929d9bc31ee1a536daebc427315fb76ef
Cathleen Ghenne <cathleen@workangel.com>
2017-06-29 19:56:11 +0100

    bunny before moving

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
-  var emptyCell = {
-    class: 'empty',
+  var carrotCell = {
+    class: 'carrot',
+    hasBunny: false,
   };
-  var bunnyCell = {
-    class: 'bunny',
+  var grassCell = {
+    class: 'grass',
+    hasBunny: false,
+  };
+  var rockCell = {
+    class: 'rock',
+    hasBunny: false,
+  };
+  var waterCell = {
+    class: 'water',
+    hasBunny: false,
+  };
+  var logCell = {
+    class: 'log',
+    hasBunny: false,
   };
   var arrayX = new Array(5);
   var arrayY = new Array(5);
   var board = [
-    [bunnyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-    [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-    [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-    [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-    [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+    [grassCell, grassCell, carrotCell, grassCell, grassCell],
+    [waterCell, waterCell, waterCell, rockCell, waterCell],
+    [logCell, logCell, waterCell, waterCell, waterCell],
+    [waterCell, rockCell, rockCell, waterCell, waterCell],
+    [grassCell, grassCell, grassCell, grassCell, grassCell],
   ];
 
   var x;
@@ -27,11 +41,8 @@ $(document).ready(function() {
     }
   }
 
-  whereIsBunny();
-  board[0][0] = emptyCell;
-  board[1][0] = bunnyCell;
-  redrawCell('#0-0', emptyCell);
-  redrawCell('#1-0', bunnyCell);
+  board[4][2].hasBunny = true;
+  redrawCell('#4-2', board[4][2]);
   whereIsBunny();
 
   function redrawCell(id, currentCell) {
@@ -39,12 +50,16 @@ $(document).ready(function() {
       .attr('class', '')
       .addClass('cell')
       .addClass(currentCell.class);
+
+    if(currentCell.hasBunny) {
+      $(id).addClass('bunny');
+    }
   }
 
   function whereIsBunny() {
     for (x = 0; x < arrayX.length; x++) {
       for (y = 0; y < arrayY.length; y++) {
-        if (board[x][y] === bunnyCell) {
+        if (board[x][y].hasBunny) {
           console.log('Bunny is on cell ' + x + ' ' + y);
         }
       }
